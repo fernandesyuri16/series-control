a36b3f7c37598edb0cd1f036181918a79358cf9d
Yuri Fernandes <yuricaparelliofc@gmail.com>
2024-07-03 21:31:22 -0300

    improving providers

---
+++ app/Providers/SeriesRepositoryProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\EloquentSeriesRepository;
+use App\Repositories\SeriesRepository;
+use Illuminate\Support\ServiceProvider;
+
+class SeriesRepositoryProvider extends ServiceProvider
+{
+    public array $bindings = [
+        SeriesRepository::class => EloquentSeriesRepository::class
+    ];
+
+}

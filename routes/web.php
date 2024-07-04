482e6265b97442dcce6b4cd6e34838d3ba2bf7e9
Yuri Fernandes <yuricaparelliofc@gmail.com>
2024-07-03 21:30:31 -0300

    creating watched episodes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\EpisodesController;
 use App\Http\Controllers\SeasonsController;
 use App\Http\Controllers\SeriesController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,3 +25,6 @@ Route::resource('/series', SeriesController::class)
     ->except(['show']);
 
 Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
+
+Route::get('/series/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
+Route::post('/series/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');

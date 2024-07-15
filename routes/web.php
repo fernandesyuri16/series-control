bb5c73e80ba27701878016ab2e7cf892e7a86a73
Yuri Fernandes <yuricaparelliofc@gmail.com>
2024-07-15 20:38:02 -0300

    compose update

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\EpisodesController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\SeasonsController;
 use App\Http\Controllers\SeriesController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\UsersController;
+use App\Http\Middleware\Authenticator;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,14 +19,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect('/series');
-});
-
 Route::resource('/series', SeriesController::class)
     ->except(['show']);
 
-Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
+Route::middleware('authenticator')->group(function () {
+    Route::get('/', function () {
+        return redirect('/series');
+    });
 
-Route::get('/series/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
-Route::post('/series/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
+    Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])
+        ->name('seasons.index');
+
+    Route::get('/series/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
+    Route::post('/series/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
+});
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store'])->name('signin');
+Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
+
+Route::get('/register', [UsersController::class, 'create'])->name('users.create');
+Route::post('/register', [UsersController::class, 'store'])->name('users.store');
